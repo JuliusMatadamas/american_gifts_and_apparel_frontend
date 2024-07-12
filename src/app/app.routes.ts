bcf34aa72cdc0ffc68381c496956370c7c7458b4
Julio Cesar Matadamas <julius3877@gmail.com>
2024-07-11 19:39:58 -0500

    Se crear la pagina de error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {ErrorComponent} from "./pages/error/error.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'error',
+    component: ErrorComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    pathMatch: "full",
+    redirectTo: 'error'
+  }
+];
